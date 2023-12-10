da8a613b38cc4e65a8faed7ee0cabf3426858065
Pit <sinningpit@gmail.com>
2023-12-10 18:06:40 +0100

    added testuser on database creation

--- recommender.py
+++ recommender.py
@@ -6,6 +6,8 @@ from flask_user import login_required, UserManager
 from models import db, User, Movie, MovieGenre
 from read_data import check_and_read_data
 
+from datetime import datetime
+
 # Class-based application configuration
 class ConfigClass(object):
     """ Flask application config """
@@ -32,12 +34,27 @@ db.create_all()  # create database if necessary
 user_manager = UserManager(app, db, User)  # initialize Flask-User management
 
 
+def create_test_user():
+    from werkzeug.security import generate_password_hash
+
+    # Test123
+    hashed_password = '$2b$12$2PbFYnIt5NSfYIaVxSrxmOiDGbpvgc.RBNHhEs5QPCRYzn/bHTrfe'
+    test_user = User(username='testuser', password=hashed_password)
+
+    db.session.add(test_user)
+    db.session.commit()
+    print(f'Created test user with username: {test_user.username} and password: Test123')
+
+
 @app.cli.command('initdb')
 def initdb_command():
     global db
     """Creates the database tables."""
     check_and_read_data(db)
     print('Initialized the database.')
+    
+    create_test_user()
+
 
 # The Home page is accessible to anyone
 @app.route('/')
@@ -51,6 +68,9 @@ def home_page():
 @login_required  # User must be authenticated
 def movies_page():
     # String-based templates
+
+    # show list of genres
+    genres = MovieGenre.query.distinct(MovieGenre.genre).all()
 
     # first 10 movies
     movies = Movie.query.limit(10).all()
